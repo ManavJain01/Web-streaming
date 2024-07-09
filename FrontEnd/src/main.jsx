35c9e7392910489525503d2cc8b1b42a3df4b414
Manav Jain <118716368+ManavJain01@users.noreply.github.com>
2024-07-09 19:23:36 +0530

    Rearranging frontend pages

--- FrontEnd/src/main.jsx
+++ FrontEnd/src/main.jsx
@@ -1,6 +1,11 @@
+// Importing React Packages
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+
+// Importing React Redux
+import { Provider } from "react-redux";
+import store from './redux/store';
 
 // Importing Chakra Components
 import { ChakraProvider } from '@chakra-ui/react'
@@ -10,21 +15,22 @@ import Layout from './Layout.jsx'
 import { theme } from './theme/theme.jsx'
 
 // Importing Local Components
+import Login from './pages/Login/Login.jsx'
 import App from './App.jsx'
-import Movies_TV from './Movies&TV/components/index.jsx'
-import Upload from './Upload/components/Index.jsx'
+import Movies_TV from './pages/Movies&TV/components/index.jsx'
+import Upload from './pages/Upload/components/Index.jsx'
 
 const router = createBrowserRouter([
   {
     path: '/',
+    element: <Login />
+  },
+  {
+    path: 'Home',
     element: <Layout />,
     children : [
       {
         path: "",
-        element : <App />
-      },
-      {
-        path: "Home",
         element : <App />
       },
       {
@@ -41,8 +47,10 @@ const router = createBrowserRouter([
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <ChakraProvider theme={theme}>
-      <RouterProvider router={router} />
-    </ChakraProvider>
+    <Provider store={store}>
+      <ChakraProvider theme={theme}>
+        <RouterProvider router={router} />
+      </ChakraProvider>
+    </Provider>
   </React.StrictMode>,
 )
---
+++ FrontEnd/src/pages/Login/Login.jsx
@@ -0,0 +1,8 @@
+// Import React Packages
+import { useState } from 'react'
+
+export default function Login() {
+  return (
+    <div>Login</div>
+  )
+}
